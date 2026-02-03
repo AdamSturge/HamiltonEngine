5dad79c4c271ec2dcbdc4c8de5e932503022b634
Adam Sturge <adamsturge137@gmail.com>
2026-02-02 18:34:32 -0800

    add simple PhysicsState component

--- HamiltonEngine/Main.cpp
+++ HamiltonEngine/Main.cpp
@@ -1,7 +1,22 @@
+
+#include "Physics/PhysicsComponent.h"
+
 #include "entt/entt.hpp"
 #include <Eigen/Dense>
 
+namespace HamiltonEngine
+{
+	void CreateEntities(entt::registry& registry)
+	{
+		entt::entity entity = registry.create();
+		registry.emplace<Physics::PhysicsState>(entity);
+	}
+}
+
 int main(int argc, char** argv)
 {
+	entt::registry registry;
+	HamiltonEngine::CreateEntities(registry);
+
 	return 0;
 }
