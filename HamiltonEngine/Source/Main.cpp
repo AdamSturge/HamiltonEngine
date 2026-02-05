8b39709f83082ca1676d1bcc83c51a3b0ed708be
Adam Sturge <adamsturge137@gmail.com>
2026-02-04 18:32:12 -0800

    kept only string overloads

--- HamiltonEngine/Source/Main.cpp
+++ HamiltonEngine/Source/Main.cpp
@@ -16,8 +16,6 @@ namespace HamiltonEngine
 		entt::entity entity = registry.create();
 		registry.emplace<Physics::PhysicsComponent>(entity);
 	}
-
-	ConfigurationVariable<int> TestVar("TestKey", 1);
 }
 
 int main(int argc, char** argv)
@@ -26,8 +24,6 @@ int main(int argc, char** argv)
 
 	entt::registry registry;
 	HamiltonEngine::CreateEntities(registry);
-
-	std::cout << HamiltonEngine::TestVar << std::endl;
 
 	glfwInit(); // Initialize OpenGL
 	GLFWwindow* window = HamiltonEngine::OpenGL::createWindow(800, 600, "MyWindow");
