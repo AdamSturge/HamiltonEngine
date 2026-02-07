4f5d451a056918f7c2ef6d506341e01dc11f0419
Adam Sturge <adamsturge137@gmail.com>
2026-02-07 11:12:27 -0800

    fixed error in json array

--- HamiltonEngine/Source/Configuration/Config.h
+++ HamiltonEngine/Source/Configuration/Config.h
@@ -1,12 +1,11 @@
 #pragma once
 
 #include <iostream> //TODO replace with final logging library. Used for json exceptions
-//#include <Configuration/ConfigurationVariable.h>
 
 //from nlohmann json
-#define JSON_TRY_USER if(true)
-#define JSON_CATCH_USER(exception) if(false)
-#define JSON_THROW_USER(exception)                           \
-    {std::clog << "Error in " << __FILE__ << ":" << __LINE__ \
-               << " (function " << __FUNCTION__ << ") - "    \
-               << (exception).what() << std::endl;}
+ #define JSON_TRY_USER if(true)
+ #define JSON_CATCH_USER(exception) if(false)
+ #define JSON_THROW_USER(exception)                           \
+     {std::clog << "Error in " << __FILE__ << ":" << __LINE__ \
+                << " (function " << __FUNCTION__ << ") - "    \
+                << (exception).what() << std::endl;}
