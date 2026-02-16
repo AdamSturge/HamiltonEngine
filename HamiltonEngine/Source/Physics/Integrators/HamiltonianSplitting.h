d04005fbe2179a2b4662b951263bb943af3f9d72
Adam Sturge <adamsturge137@gmail.com>
2026-02-16 13:32:06 -0800

    comment cleanup

--- HamiltonEngine/Source/Physics/Integrators/HamiltonianSplitting.h
+++ HamiltonEngine/Source/Physics/Integrators/HamiltonianSplitting.h
@@ -18,7 +18,6 @@ namespace HamiltonEngine::Physics
 	//B are the weights for the kinentic energy split  H = b0T + b1T (b0 + b1 = 1)
 	//C are the weights for the potnential energy dT substepping Phi_{V,dt} = Phi_{V,c1*dt} o Phi_{V,c0*dt} (c0 + c1 = 1)
 	//D are the weights for the kinetic energy dT substepping Phi_{V,dt} = Phi_{T,d1*dt} o Phi_{T,d0*dt} (d0 + d1 = 1)
-	//Each splitting step runs 2 steps of either potential or kinematic integration
 	template<int N, int M>
 	void HamiltonianSplitting(const float A[N], 
 		const float B[M],
@@ -50,6 +49,8 @@ namespace HamiltonEngine::Physics
 				}
 				case EulerMode::KineticOnly:
 				{
+					//Should never wrap but do mod just to avoid crashing in
+					//case of bad inputs
 					const int ModIndex = Index % M;
 					const float KineticDt = D[ModIndex] * Dt;
 					EulerKineticOnly(Mass,
@@ -62,7 +63,7 @@ namespace HamiltonEngine::Physics
 				default: 
 				{
 					//LOG this
-				}//LOG this
+				}
 			}
 
 		}
--- HamiltonEngine/Source/Physics/Integrators/StormerVerlet.h
+++ HamiltonEngine/Source/Physics/Integrators/StormerVerlet.h
@@ -3,10 +3,11 @@
 #include "Configuration/Globals.h"
 
 /*
-	StormerVerlet is a version of symplectic Euler with the folowing update rule given a Hamiltonian
-	H = 0.5V + T + 0.5V
-	p_{n+1} = p_{n} - dt*GradV(q_n) <-- dp/dt = -dH/dq = -GradV(q)
-	q_{n+1} = q_{n} + dt+p_{n+1}    <-- dq/dt = -dH/dp = dT/dp = p
+	StormerVerlet has the folowing update rule:
+	Hamiltonian H = 0.5V + T + 0.5V
+	p_{n+0.5} = p_{n} - 0.5f*dt*GradV(q_n)
+	q_{n+1} = q_{n} + dt+p_{n+0.5}
+	p_{n+1} = p_{n+0.5} - 0.5f*dt*GradV(q_{n+1})
 */
 
 namespace HamiltonEngine::Physics
