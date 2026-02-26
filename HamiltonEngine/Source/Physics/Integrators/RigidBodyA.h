d8b89744d7dcb20b409b5de85be134f6bbea57c2
Adam Sturge <adamsturge137@gmail.com>
2026-02-25 16:40:11 -0800

    wrote some comments about the theory of rigid body integration

--- HamiltonEngine/Source/Physics/Integrators/RigidBodyA.h
+++ HamiltonEngine/Source/Physics/Integrators/RigidBodyA.h
@@ -3,10 +3,30 @@
 #include "Configuration/Globals.h"
 
 /*
-	EulerA is a version of symplectic Euler with the folowing update rule given a Hamiltonian
+	For a free rigid body (ie only kinetic contributions to the hamiltonian)
+	the differential equation describing the evolution of the angular momentum b, in body coordinates, is:
+	db/dt = b x (I^-1) b 
+	where I is the inertia tensor.
+	If we consider only the X part of the angular momentum this cross product can be written
+	dl/dt = A_x b 
+	for a special matrix A_x. This can be explicitly solved for b(t)
+	b(t) = exp(A_x t) b(0)
+	Similar matricies can be found for Y and Z.
+
+	Writing the columns of the orientation matrix as s_x,s_y_,s_z we can apply the same treatment to the position upate
+	ds/dt = s x (I^-1) b
+	The solution to this differential equation is the same as above
+	s(t) = exp(A_x t) s(0).
+
+	Thus we can compose 3 steps, one for X,Y,and Z, to fully integrate the kinetic part of the hamiltonian
+
+	Moving on to the Potential portion of the update we must now consider the effects of a potential
+	on a particular portion of the rigid body
+
+	TODO finish this once I have a better idea of how it works
+
+	RigidBodyA is a Rigid Body integration technique based on EulerA with the folowing update rule given a Hamiltonian
 	H = T + V
-	q_{n+1} = q_{n} + dt+p_{n}    <-- dq/dt = -dH/dp = dT/dp = p
-	p_{n+1} = p_{n} - dt*GradV(q_{n+1}) <-- dp/dt = -dH/dq = -GradV(q)
 */
 
 namespace Eigen
--- HamiltonEngine/Source/Physics/Integrators/RigidBodyB.h
+++ HamiltonEngine/Source/Physics/Integrators/RigidBodyB.h
@@ -3,10 +3,9 @@
 #include "Configuration/Globals.h"
 
 /*
-	EulerA is a version of symplectic Euler with the folowing update rule given a Hamiltonian
-	H = T + V
-	q_{n+1} = q_{n} + dt+p_{n}    <-- dq/dt = -dH/dp = dT/dp = p
-	p_{n+1} = p_{n} - dt*GradV(q_{n+1}) <-- dp/dt = -dH/dq = -GradV(q)
+	RigidBodyB is a Rigid Body integration technique based on EulerB
+
+	See RigidBodyA for more details
 */
 
 namespace Eigen
